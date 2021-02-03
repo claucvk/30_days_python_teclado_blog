4fde335924a484983082941a4bedf1166ed1cca3
Claudia El Khatib <claudiacv22@gmail.com>
2021-02-03 17:05:52 -0500

    Python exercises from day 1 to day 14

---
+++ .ipynb_checkpoints/day1-checkpoint.py
@@ -0,0 +1,13 @@
+# Calculate and print the number of days, weeks, and months in 27 years. Don’t worry about leap years!
+print(f"Number of months in 27 years: {27 * 12} months")
+print(f"Number of weeks in 27 years: {(4 * 12) * 27} weeks")
+print(f"Number of days in 27 years: {365 * 27} days")
+
+# Calculate and print the area of a circle with a radius of 9 units. You can be as accurate as you like with the value of pi. Result= 254
+print((9 * 9) * 3.14)
+
+# Radius 5 
+print((5 * 5) * 3.141)
+
+print(3.141 * 5 ** 2) # operator for exponent
+print(pow(5,2) * 3.141) 
